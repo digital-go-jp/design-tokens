4c995e78e93aa78f45b192ebcd9ee39719f69bd2
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-12-07 11:01:43 +0900

    use biome

--- style-dictionary/transform.ts
+++ style-dictionary/transform.ts
@@ -1,61 +1,61 @@
-import * as StyleDictionary from 'style-dictionary';
-import config from './config.json';
+import * as StyleDictionary from 'style-dictionary'
+import config from './config.json'
 
 StyleDictionary.registerTransform({
   name: 'px-rem-transformer',
   type: 'value',
-  matcher: (token) => {
-    return token.name.includes('font-size') || token.name.includes('FontSize');
+  matcher: token => {
+    return token.name.includes('font-size') || token.name.includes('FontSize')
   },
-  transformer: (token) => {
-    const px = Number(token.value.replace('px', ''));
-    const rem = (1 / 16) * px + 'rem';
+  transformer: token => {
+    const px = Number(token.value.replace('px', ''))
+    const rem = `${(1 / 16) * px}rem`
     if (token.name.includes('font-size') || token.name.includes('FontSize')) {
-      return rem;
+      return rem
     }
-    return token.value;
+    return token.value
   },
-});
+})
 
 StyleDictionary.registerTransform({
   name: 'shadow-transformer',
   type: 'value',
-  matcher: function (prop) {
-    return prop.type === 'boxShadow';
+  matcher: prop => {
+    return prop.type === 'boxShadow'
   },
-  transformer: function (token) {
-    const v1 = token.value[0];
-    const v2 = token.value[1];
+  transformer: token => {
+    const v1 = token.value[0]
+    const v2 = token.value[1]
 
-    return `${v1.x} ${v1.y} ${v1.blur} ${v1.spread} ${v1.color}, ${v2.x} ${v2.y} ${v2.blur} ${v2.spread} ${v2.color}`;
+    return `${v1.x} ${v1.y} ${v1.blur} ${v1.spread} ${v1.color}, ${v2.x} ${v2.y} ${v2.blur} ${v2.spread} ${v2.color}`
   },
-});
+})
 
 StyleDictionary.registerTransformGroup({
   name: 'da/css',
-  transforms: (StyleDictionary.transformGroup['css'] ?? []).concat([
+  transforms: (StyleDictionary.transformGroup.css ?? []).concat([
     'px-rem-transformer',
     'shadow-transformer',
   ]),
-});
+})
 
 StyleDictionary.registerTransformGroup({
   name: 'da/ts',
-  transforms: (StyleDictionary.transformGroup['js'] ?? []).concat([
+  transforms: (StyleDictionary.transformGroup.js ?? []).concat([
     'px-rem-transformer',
     'shadow-transformer',
   ]),
-});
+})
 
 StyleDictionary.registerFilter({
   name: 'da/filter-tokens',
-  matcher: (token) => {
+  matcher: token => {
     if (token.name.includes('token-set') || token.name.includes('TokenSet')) {
-      return false;
+      return false
     }
 
-    return true;
+    return true
   },
-});
+})
 
-StyleDictionary.extend(config).buildAllPlatforms();
+StyleDictionary.extend(config).buildAllPlatforms()
