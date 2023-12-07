7d89f93d2da7337ab26497722ff2cf361312877b
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-12-07 14:29:44 +0900

    run formatter

--- style-dictionary/transform.ts
+++ style-dictionary/transform.ts
@@ -1,35 +1,35 @@
-import * as StyleDictionary from 'style-dictionary'
-import config from './config.json'
+import * as StyleDictionary from 'style-dictionary';
+import config from './config.json';
 
 StyleDictionary.registerTransform({
   name: 'px-rem-transformer',
   type: 'value',
   matcher: token => {
-    return token.name.includes('font-size') || token.name.includes('FontSize')
+    return token.name.includes('font-size') || token.name.includes('FontSize');
   },
   transformer: token => {
-    const px = Number(token.value.replace('px', ''))
-    const rem = `${(1 / 16) * px}rem`
+    const px = Number(token.value.replace('px', ''));
+    const rem = `${(1 / 16) * px}rem`;
     if (token.name.includes('font-size') || token.name.includes('FontSize')) {
-      return rem
+      return rem;
     }
-    return token.value
+    return token.value;
   },
-})
+});
 
 StyleDictionary.registerTransform({
   name: 'shadow-transformer',
   type: 'value',
   matcher: prop => {
-    return prop.type === 'boxShadow'
+    return prop.type === 'boxShadow';
   },
   transformer: token => {
-    const v1 = token.value[0]
-    const v2 = token.value[1]
+    const v1 = token.value[0];
+    const v2 = token.value[1];
 
-    return `${v1.x} ${v1.y} ${v1.blur} ${v1.spread} ${v1.color}, ${v2.x} ${v2.y} ${v2.blur} ${v2.spread} ${v2.color}`
+    return `${v1.x} ${v1.y} ${v1.blur} ${v1.spread} ${v1.color}, ${v2.x} ${v2.y} ${v2.blur} ${v2.spread} ${v2.color}`;
   },
-})
+});
 
 StyleDictionary.registerTransformGroup({
   name: 'da/css',
@@ -37,7 +37,7 @@ StyleDictionary.registerTransformGroup({
     'px-rem-transformer',
     'shadow-transformer',
   ]),
-})
+});
 
 StyleDictionary.registerTransformGroup({
   name: 'da/ts',
@@ -45,17 +45,17 @@ StyleDictionary.registerTransformGroup({
     'px-rem-transformer',
     'shadow-transformer',
   ]),
-})
+});
 
 StyleDictionary.registerFilter({
   name: 'da/filter-tokens',
   matcher: token => {
     if (token.name.includes('token-set') || token.name.includes('TokenSet')) {
-      return false
+      return false;
     }
 
-    return true
+    return true;
   },
-})
+});
 
-StyleDictionary.extend(config).buildAllPlatforms()
+StyleDictionary.extend(config).buildAllPlatforms();
