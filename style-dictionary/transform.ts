2f8ff89bafa8917bef257e27e1462f4877c33b2c
takumi-hatta-dig <TakHatta@digital.go.jp>
2024-11-07 19:46:57 +0900

    upgrade style dictionary

--- style-dictionary/transform.ts
+++ style-dictionary/transform.ts
@@ -1,61 +1,24 @@
-import * as StyleDictionary from 'style-dictionary';
-import config from './config.json';
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
 
-StyleDictionary.registerTransform({
-  name: 'px-rem-transformer',
-  type: 'value',
-  matcher: (token) => {
-    return token.name.includes('font-size') || token.name.includes('FontSize');
-  },
-  transformer: (token) => {
-    const px = Number(token.value.replace('px', ''));
-    const rem = `${(1 / 16) * px}rem`;
-    if (token.name.includes('font-size') || token.name.includes('FontSize')) {
-      return rem;
-    }
-    return token.value;
-  },
-});
-
-StyleDictionary.registerTransform({
-  name: 'shadow-transformer',
-  type: 'value',
-  matcher: (prop) => {
-    return prop.type === 'boxShadow';
-  },
-  transformer: (token) => {
-    const v1 = token.value[0];
-    const v2 = token.value[1];
-
-    return `${v1.x} ${v1.y} ${v1.blur} ${v1.spread} ${v1.color}, ${v2.x} ${v2.y} ${v2.blur} ${v2.spread} ${v2.color}`;
-  },
-});
-
-StyleDictionary.registerTransformGroup({
-  name: 'da/css',
-  transforms: (StyleDictionary.transformGroup.css ?? []).concat([
-    'px-rem-transformer',
-    'shadow-transformer',
-  ]),
-});
-
-StyleDictionary.registerTransformGroup({
-  name: 'da/ts',
-  transforms: (StyleDictionary.transformGroup.js ?? []).concat([
-    'px-rem-transformer',
-    'shadow-transformer',
-  ]),
-});
+register(StyleDictionary);
 
 StyleDictionary.registerFilter({
-  name: 'da/filter-tokens',
-  matcher: (token) => {
-    if (token.name.includes('token-set') || token.name.includes('TokenSet')) {
-      return false;
+  name: 'tokens-filter',
+  filter: (token) => {
+    if (token.path.length === 0) {
+      return true;
     }
-
-    return true;
+    return token.path[0] !== 'tokenSetOrder';
   },
 });
 
-StyleDictionary.extend(config).buildAllPlatforms();
+const sd = new StyleDictionary('style-dictionary/config.json');
+
+const run = async (sd: StyleDictionary) => {
+  await sd.hasInitialized;
+  await sd.cleanAllPlatforms();
+  await sd.buildAllPlatforms();
+};
+
+run(sd);
